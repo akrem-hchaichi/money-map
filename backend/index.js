84fd494982cac5f6dff3f6b0559e5c42f3ac4b2a
Akrem Hchaichi <hchaichi-akrem@outlook.fr>
2024-08-16 12:08:47 +0100

    deployment setup completed

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import passport from "passport";
 import session from "express-session";
 import connectMongo from "connect-mongodb-session";
+import path from "path";
 
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from "@apollo/server/express4";
@@ -19,6 +20,7 @@ import { configurePassport } from "./passport/passport.config.js";
 dotenv.config();
 configurePassport();
 
+const __dirname = path.resolve();
 
 const app = express();
 const httpServer = http.createServer(app);
@@ -68,6 +70,12 @@ app.use(
     context: async ({ req, res }) => buildContext({ req, res }),
   })
 );
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
+});
 
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 await connectDB();
